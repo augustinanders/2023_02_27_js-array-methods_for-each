1a763447736181f6ed11e3e2911642c9d8c39c7f
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-02-27 19:10:01 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const colors = [
+  "#343434",
+  "#7FB5B5",
+  "#633A34",
+  "#354D73",
+  "#F3A505",
+  "#2A6478",
+  "#CC0605",
+  "#497E76",
+  "#D36E70",
+  "#FF2301",
+  "#4C514A",
+  "#2E3A23",
+  "#5D9B9B",
+  "#474A51",
+  "#EFA94A",
+  "#4D5645",
+  "#606E8C",
+  "#A5A5A5",
+];
+
+const body = document.querySelector('[data-js="body"]');
+
+colors.forEach((color) => {
+  console.log(color);
+  const colorContainer = document.createElement("div");
+  colorContainer.style.backgroundColor = color;
+  colorContainer.classList.add("color-box");
+  body.append(colorContainer);
+});
